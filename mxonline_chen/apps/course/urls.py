481e7718c8e58790a9399b67e62234901435986c
cynpythoncyn <cyn1105690826@aliyun.com>
2018-11-16 11:55:25 +0800

    课程详情页显示

--- mxonline_chen/apps/course/urls.py
+++ mxonline_chen/apps/course/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
 
-from .views import CourselistView
+from .views import CourselistView,CoursedetailView
 
 urlpatterns = [
     # 课程列表页url
     url(r'^list/$', CourselistView.as_view(), name='list'),
+    url(r'^detail/(?P<course_id>\d+)/$', CoursedetailView.as_view(), name='detail'),
 ]
--- mxonline_chen/apps/course/views.py
+++ mxonline_chen/apps/course/views.py
@@ -5,6 +5,47 @@ from django.views.generic.base import View
 from pure_pagination import Paginator,PageNotAnInteger
 
 from .models import Course
+from operation.models import UserFavorite
+
+class CoursedetailView(View):
+    """
+    课程详情页
+    """
+    def get(self,request,course_id):
+
+        all_course = Course.objects.get(id=int(course_id))
+
+        # 增加课程点击数
+        all_course.click_nums += 1
+        all_course.save()
+
+        # 是否收藏课程
+        has_fav_course = False
+        # 是否收藏机构
+        has_fav_org = False
+        if request.user.is_authenticated():
+            if UserFavorite.objects.filter(user=request.user,fav_id=all_course.id,fav_type=1):
+                has_fav_course = True
+
+            if UserFavorite.objects.filter(user=request.user,fav_type=2,fav_id=all_course.course_org.id):
+                has_fav_org = True
+        # 相关课程推荐
+        tag = all_course.tag
+        if tag:
+
+            relate_courses = Course.objects.filter(tag=tag)[:1]
+        else:
+            relate_courses = []
+
+        return render(request,'course-detail.html',{
+            'course':all_course,
+            'relate_courses':relate_courses,
+            'has_fav_course':has_fav_course,
+            'has_fav_org':has_fav_org,
+        })
+
+
+
 class CourselistView(View):
     """
     课程列表页
